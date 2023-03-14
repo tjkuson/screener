fb453c5236b0d367e8ea86393b7d12d80b50119f
Tom Kuson <mail@tjkuson.me>
2023-03-14 16:47:02 +0000

    Add feature

--- screener/parser/epub.py
+++ screener/parser/epub.py
@@ -5,16 +5,18 @@ from pathlib import Path
 from ebooklib import ITEM_DOCUMENT
 
 from screener.reader import EpubFileReader
-from screener.utils import html_contains_javascript
+from screener.utils import html_contains_javascript, html_contains_images_with_external_sources
 
 
 def parse_epub(path_to_epub: Path) -> bool:
     """Parse epub to check that it is safe."""
     with EpubFileReader(path_to_epub) as epub:
-        contains_js: bool = False
 
         for item in epub.book.get_items_of_type(ITEM_DOCUMENT):
             content: bytes = item.get_content()
             if html_contains_javascript(content):
-                contains_js = True
-        return not contains_js
+                return True
+            if html_contains_images_with_external_sources(content):
+                return True
+
+    return False
