cbf0dc27d1b8cdb63bef577ec503a09ae3cacc67
Tom Kuson <mail@tjkuson.me>
2023-03-14 15:34:12 +0000

    add check images function

--- screener/tests/test_utils.py
+++ screener/tests/test_utils.py
@@ -2,7 +2,10 @@
 
 from pathlib import Path
 
-from screener.utils import html_contains_javascript
+from screener.utils import (
+    html_contains_images_with_external_sources,
+    html_contains_javascript,
+)
 
 TEST_DIR = Path(__file__).parent
 
@@ -18,3 +21,16 @@ def test_html_contains_javascript() -> None:
     with open(html_with_script_tags, "rb") as file:
         content = file.read()
         assert html_contains_javascript(content)
+
+
+def test_html_contains_images_with_external_sources() -> None:
+    """Test html_contains_images_with_external_sources function."""
+    safe_html = TEST_DIR / "safe_image.html"
+    with open(safe_html, "rb") as file:
+        content: bytes = file.read()
+        assert not html_contains_images_with_external_sources(content)
+
+    html_with_external_image = TEST_DIR / "unsafe_image.html"
+    with open(html_with_external_image, "rb") as file:
+        content = file.read()
+        assert html_contains_images_with_external_sources(content)
