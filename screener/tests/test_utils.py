b8a77f63fe192b5f7ee6d3b4db017c95aa870f6c
Tom Kuson <12143269+tjkuson@users.noreply.github.com>
2023-05-12 16:56:03 +0100

    nit

--- screener/tests/test_utils.py
+++ screener/tests/test_utils.py
@@ -19,13 +19,13 @@ class TestUtilFunctions:
         """Test html_contains_javascript function."""
         # Check function returns False when there are no script tags
         safe_html = self.test_dir / "safe.html"
-        with open(safe_html, "rb") as file:
+        with safe_html.open("rb") as file:
             content: bytes = file.read()
             assert not html_contains_javascript(content)
 
         # Check function returns True when there are script tags
         html_with_script_tags = self.test_dir / "script_tags.html"
-        with open(html_with_script_tags, "rb") as file:
+        with html_with_script_tags.open("rb") as file:
             content = file.read()
             assert html_contains_javascript(content)
 
@@ -35,12 +35,12 @@ class TestUtilFunctions:
         """Test html_contains_images_with_external_sources function."""
         # Check function returns False when there are external images
         safe_html = self.test_dir / "safe_image.html"
-        with open(safe_html, "rb") as file:
+        with safe_html.open("rb") as file:
             content = file.read()
             assert not html_contains_images_with_external_sources(content)
 
         # Check function returns True when there are external images
         html_with_external_image = self.test_dir / "unsafe_image.html"
-        with open(html_with_external_image, "rb") as file:
+        with html_with_external_image.open("rb") as file:
             content = file.read()
             assert html_contains_images_with_external_sources(content)
