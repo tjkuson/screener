2c64106ff0fbf4fb27e0e4a0ac0dcd1f377f3025
Tom Kuson <mail@tjkuson.me>
2023-05-22 10:56:55 +0100

    Clean up

--- screener/reader/epub.py
+++ screener/reader/epub.py
@@ -2,8 +2,9 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
 import warnings
+from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from pathlib import Path
 
--- screener/tests/hey.py
+++
@@ -1,7 +0,0 @@
-from ebooklib import epub
-
-def main():
-    book = epub.read_epub("william-shakespeare_richard-ii.epub", options={"ignore_ncx": True})
-
-if __name__ == "__main__":
-    main()
