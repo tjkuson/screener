981bd5922fd97e1090f481a33755fbc3e28f56d1
Tom Kuson <mail@tjkuson.me>
2023-07-28 15:56:24 +0100

    Add culprit info to msg

--- screener/diagnostic.py
+++ screener/diagnostic.py
@@ -13,18 +13,30 @@ class Diagnostic:
 class JavaScriptDiagnostic(Diagnostic):
     """Diagnostic information about a file with JavaScript."""
 
+    culprit: str
+
     def __str__(self) -> str:
         """Return a string representation of the diagnostic."""
-        return f"JavaScript found in {self.file_name}"
+        return (
+            f"JavaScript found in {self.file_name}: {self.culprit}"
+            if self.culprit
+            else f"JavaScript found in {self.file_name}"
+        )
 
 
 @dataclass
 class ExternalImageDiagnostic(Diagnostic):
     """Diagnostic information about a file with external images."""
 
+    culprit: str
+
     def __str__(self) -> str:
         """Return a string representation of the diagnostic."""
-        return f"External images found in {self.file_name}"
+        return (
+            f"External images found in {self.file_name}: {self.culprit}"
+            if self.culprit
+            else f"External images found in {self.file_name}"
+        )
 
 
 @dataclass
