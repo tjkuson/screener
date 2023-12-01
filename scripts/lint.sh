1eff5e81c0bb8407a750042dee6395e7d205682f
Tom Kuson <mail@tjkuson.me>
2023-12-01 19:04:25 +0000

    Use Ruff as formatter; remove Black

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,11 +3,11 @@
 echo "Checking with ssort..."
 python3 -m ssort --check .
 
-echo "Running ruff violation checks..."
-python3 -m ruff check --quiet .
+echo "Checking with Ruff..."
+python3 -m ruff --quiet .
 
 echo "Checking with black..."
-python3 -m black --check .
+python3 -m ruff format --check .
 
 echo "Checking with mypy..."
 python3 -m mypy --strict .
